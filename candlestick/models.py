4abf9dbca6b2228474177a2cc496c955aa5bd709
chirag.mandaviya <chirag.mandaviya12@gmail.com>
2025-05-22 21:48:57 +0530

    add logic for fetch data from upstox

--- candlestick/models.py
+++ candlestick/models.py
@@ -8,6 +8,11 @@ class Stock(models.Model):
     sector = models.CharField(max_length=255)
     isin_code = models.CharField(max_length=255, null=True, blank=True)
 
+    objects = models.Manager()
+
+    def __str__(self):
+        return str(self.symbol)
+
 
 class OHLCData(models.Model):
     data_date = models.DateField()
@@ -17,16 +22,24 @@ class OHLCData(models.Model):
     low_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name="ohlc_data")
 
+    objects = models.Manager()
+
 
 class Hammer(models.Model):
     data_date = models.DateField()
     stocks = models.ManyToManyField(Stock)
+
+    objects = models.Manager()
 
 
 class InvertedHammer(models.Model):
     data_date = models.DateField()
     stocks = models.ManyToManyField(Stock)
 
+    objects = models.Manager()
+
 
 class UpatoxAccessToken(models.Model):
     token = models.TextField()
+
+    objects = models.Manager()
--- candlestick/views.py
+++ candlestick/views.py
@@ -1,11 +1,15 @@
 import csv
 import io
 import logging
+import time
+from datetime import datetime
 
 import requests
 from django.db import transaction
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
 from rest_framework import parsers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,13 +21,58 @@ from stock_screener.settings import (
     REDIRCT_URL,
 )
 
-from .models import Stock, UpatoxAccessToken
+from .models import OHLCData, Stock, UpatoxAccessToken
+
+
+def refresh_candlestick_data(start_date, end_date):
+    logger = logging.getLogger("stock_screener_logger")
+    try:
+        stocks = Stock.objects.all()
+        access_token = UpatoxAccessToken.objects.all()[0].token
+
+        logger.info("OHLC Data fetch Starting..")
+
+        OHLCData.objects.all().delete()
+
+        for stock in stocks:
+            url = f"https://api.upstox.com/v3/historical-candle/{stock.isin_code}/days/1/{end_date}/{start_date}"
+            headers = {"Accept": "application/json", "Authorization": access_token}
+            payload = {}
+            response = requests.get(url, headers=headers, data=payload, timeout=120)
+            response_data = response.json()
+
+            if response_data.get("status") == "success":
+                candle_data = response_data.get("data")
+                ohlc_objects = []
+
+                for ohlc in candle_data.get("candles"):
+                    ohlc_objects.append(
+                        OHLCData(
+                            data_date=datetime.fromisoformat(ohlc[0]).date(),
+                            open_price=ohlc[1],
+                            high_price=ohlc[2],
+                            low_price=ohlc[3],
+                            close_price=ohlc[4],
+                            stock=stock,
+                        )
+                    )
+                if ohlc_objects:
+                    OHLCData.objects.bulk_create(ohlc_objects, batch_size=100)
+                    logger.info(f"Data for {stock.symbol} fetched")
+
+            time.sleep(0.5)
+        logger.info("OHLC Data fetched Successfully")
+        return "Success"
+    except Exception as e:
+        logger.error(f"Error : {e}", exc_info=True)
+        return "Error"
 
 
 def home_view(request):
     return render(request=request, template_name="home.html")
 
 
+@csrf_protect
 def candlestickpatterns_view(request):
     patterns = [
         "Hammer",
@@ -36,8 +85,33 @@ def candlestickpatterns_view(request):
         "Bearish Kicker",
         "Bearish Engulfing",
     ]
+    if request.method == "POST":
+        start_date = request.POST.get("start_date")
+        end_date = request.POST.get("end_date")
+
+        if start_date and end_date:
+            if start_date.strip() and end_date.strip():
+                result = refresh_candlestick_data(
+                    start_date=start_date, end_date=end_date
+                )
+                # Store result in session temporarily
+                request.session["result"] = result
+                return redirect(reverse("CandleStick"))
+
+    # This is the GET section — safely renders the page
+    result = request.session.pop("result", None)
+    message = None
+    if result:
+        message = (
+            "Data Fetched Successfully!!"
+            if result == "Success"
+            else "Something went wrong!!"
+        )
+
     return render(
-        request=request, template_name="patterns.html", context={"pattern": patterns}
+        request=request,
+        template_name="patterns.html",
+        context={"pattern": patterns, "result": result, "message": message},
     )
 
 
@@ -72,7 +146,7 @@ class UploadStockDataView(APIView):
                 reader = csv.DictReader(io_string)
 
                 # Delete all old data
-                Stock.objects.all().delete()
+                Stock.objects.all().delete()  # pylint: disable=E1101
 
                 created_objects = []
                 for row in reader:
@@ -80,21 +154,23 @@ class UploadStockDataView(APIView):
                         company_name=row.get("Company Name").strip(),
                         symbol=row.get("Symbol").strip(),
                         sector=row.get("Industry").strip(),
+                        isin_code=f"NSE_EQ|{row.get("ISIN Code").strip()}",
                     )
                     stock.save()
                     created_objects.append(
                         {
-                            "company_name": stock.company_name,
-                            "symbol": stock.symbol,
-                            "sector": stock.sector,
+                            "Company_name": stock.company_name,
+                            "Symbol": stock.symbol,
+                            "Sector": stock.sector,
+                            "ISIN_Code": stock.isin_code,
                         }
                     )
-                logger.info("Stock Data Uploded Successfully")
+                logger.info("Stock(Nifty 500) Data Uploded Successfully")
                 return Response(
                     {
                         "Status": "Success",
-                        "created_count": len(created_objects),
-                        "created_objects": created_objects,
+                        "Created_count": len(created_objects),
+                        "Created_objects": created_objects,
                     },
                     status=status.HTTP_201_CREATED,
                 )
@@ -125,5 +201,5 @@ def upstox_authentication_success(request):
     )
     access_token = response.json().get("access_token")
 
-    UpatoxAccessToken.objects.create(token=access_token)
+    UpatoxAccessToken.objects.create(token=access_token)  # pylint: disable=E1101
     return render(request=request, template_name="success.html")
